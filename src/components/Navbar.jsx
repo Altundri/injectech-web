82b1345a3d57bc433d5401b712856d2cc7ea3eb3
Altundri Wahyu <altundri.hidayatullah@dataon.com>
2023-09-08 11:29:16 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,13 @@ export default function Navbar() {
         <header className="absolute inset-x-0 top-0 z-50">
             <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
+                    <a href="#" className="flex items-center space-x-5 ">
                         <img
                             className="h-8 w-auto ml-14"
                             src="../src/assets/logo.png"
                             alt=""
                         />
+                        <h1 className=' ml-30 text-2xl font-bold text-gray-900'>Injectech.</h1>
                     </a>
                 </div>
                 <div className="flex lg:hidden">
