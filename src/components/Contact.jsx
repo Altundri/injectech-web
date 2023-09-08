f198f1990b4f3971d0fd35af9a30c27d2be910b1
Altundri Wahyu <altundri.hidayatullah@dataon.com>
2023-09-08 10:00:47 +0700

    feat: change color from indigo to violet

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,5 @@
 
 import { useState } from 'react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
 
 function classNames(...classes) {
@@ -25,7 +24,7 @@ export default function Contact() {
                 />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact us</h2>
                 <p className="mt-2 text-lg leading-8 text-gray-600">
                     Aute magna irure deserunt veniam aliqua magna enim voluptate.
                 </p>
@@ -42,7 +41,7 @@ export default function Contact() {
                                 name="first-name"
                                 id="first-name"
                                 autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -56,13 +55,13 @@ export default function Contact() {
                                 name="last-name"
                                 id="last-name"
                                 autoComplete="family-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
-                            Company
+                            Institution / Company
                         </label>
                         <div className="mt-2.5">
                             <input
@@ -70,7 +69,7 @@ export default function Contact() {
                                 name="company"
                                 id="company"
                                 autoComplete="organization"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -84,7 +83,7 @@ export default function Contact() {
                                 name="email"
                                 id="email"
                                 autoComplete="email"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -100,23 +99,19 @@ export default function Contact() {
                                 <select
                                     id="country"
                                     name="country"
-                                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-4 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm"
                                 >
+                                    <option>ID</option>
                                     <option>US</option>
-                                    <option>CA</option>
                                     <option>EU</option>
                                 </select>
-                                <ChevronDownIcon
-                                    className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                                    aria-hidden="true"
-                                />
                             </div>
                             <input
                                 type="tel"
                                 name="phone-number"
                                 id="phone-number"
                                 autoComplete="tel"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -129,7 +124,7 @@ export default function Contact() {
                                 name="message"
                                 id="message"
                                 rows={4}
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                                 defaultValue={''}
                             />
                         </div>
@@ -140,8 +135,8 @@ export default function Contact() {
                                 checked={agreed}
                                 onChange={setAgreed}
                                 className={classNames(
-                                    agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                                    agreed ? 'bg-violet-600' : 'bg-gray-200',
+                                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600'
                                 )}
                             >
                                 <span className="sr-only">Agree to policies</span>
@@ -156,7 +151,7 @@ export default function Contact() {
                         </div>
                         <Switch.Label className="text-sm leading-6 text-gray-600">
                             By selecting this, you agree to our{' '}
-                            <a href="#" className="font-semibold text-indigo-600">
+                            <a href="#" className="font-semibold text-violet-600">
                                 privacy&nbsp;policy
                             </a>
                             .
@@ -166,7 +161,7 @@ export default function Contact() {
                 <div className="mt-10">
                     <button
                         type="submit"
-                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="block w-full rounded-md bg-violet-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
                     >
                         Send message
                     </button>
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,37 +2,39 @@ import CardService from "./CardService";
 
 export default function Service() {
     return (
-        <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:text-center">
-                    <h2 className="text-2xl font-semibold leading-7 text-indigo-600">Service</h2>
-                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Layanan kami yang akan menjadi solusi bagi masalah anda
-                    </p>
-                </div>
-                <div className="grid grid-cols-4 gap-4 mt-24">
-                    <CardService
-                    title="Mobile App"
-                    text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-                    image="../src/assets/user-interface.png"
-                    />
-                    <CardService
-                    title="Website"
-                    text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-                    image="../src/assets/web-interface.png"
-                    />
-                    <CardService
-                    title="Videography"
-                    text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-                    image="../src/assets/videography.png"
-                    />
-                    <CardService
-                    title="UI/UX Designer"
-                    text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-                    image="../src/assets/uiux.png"
-                    />
-                </div>
-            </div>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:text-center">
+            <h2 className="text-2xl font-semibold leading-7 text-violet-600">
+              Service
+            </h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Layanan kami yang akan menjadi solusi bagi masalah anda
+            </p>
+          </div>
+          <div className="grid grid-cols-4 gap-4 mt-24">
+            <CardService
+              title="Mobile App"
+              text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+              image="../src/assets/user-interface.png"
+            />
+            <CardService
+              title="Website"
+              text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+              image="../src/assets/web-interface.png"
+            />
+            <CardService
+              title="Videography"
+              text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+              image="../src/assets/videography.png"
+            />
+            <CardService
+              title="UI/UX Designer"
+              text="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+              image="../src/assets/uiux.png"
+            />
+          </div>
         </div>
-    )
+      </div>
+    );
 }
