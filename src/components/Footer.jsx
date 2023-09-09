a4348229342a550896dd0abf81b2c81c51551f66
rafli_andi13 <herp1306@gmail.com>
2023-09-09 15:18:00 +0700

    linked sosmed in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,7 +36,7 @@ const navigation = {
         },
         {
             name: 'Instagram',
-            href: '#',
+            href: 'https://www.instagram.com/injec.tech/',
             icon: (props) => (
                 <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                     <path
@@ -58,7 +58,7 @@ const navigation = {
         },
         {
             name: 'GitHub',
-            href: '#',
+            href: 'https://github.com/Injectech',
             icon: (props) => (
                 <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                     <path
